2675bb625c11f8e34d7691f9b8d64a00f0566eb4
Marcus O'Flaherty <moflaherty1@sheffield.ac.uk>
2023-04-27 17:21:08 +0100

    add Store::Initialise based on stringstream rather than string filename. Return false on failure to parse a config file line (which isn't a comment or empty), rather than silently ignoring it

--- src/Store/Store.cpp
+++ src/Store/Store.cpp
@@ -9,18 +9,23 @@ bool Store::Initialise(std::string filename){
   std::ifstream file(filename.c_str());
   std::string line;
   
+  bool all_ok = true;
   if(file.is_open()){
     
     while (getline(file,line)){
       if (line.size()>0){
-	if (line.at(0)=='#')continue;
-	std::string key;
-	std::string value;
-	std::stringstream stream(line);
-	if(stream>>key>>value) m_variables[key]=value;
+        if(line.at(0)=='#') continue;
+        std::string key;
+        std::string value;
+        std::stringstream stream(line);
+        if(stream>>key>>value) m_variables[key]=value;
+        else {
+          std::cout<<"Store::Initialise failed to parse line '"<<line<<"'"<<std::endl;
+          all_ok = false;
+        }
       }
-      
     }
+    
     file.close();
   }
   else{
@@ -28,7 +33,27 @@ bool Store::Initialise(std::string filename){
     return false;
   }
   
-  return true;
+  return all_ok;
+}
+
+bool Store::Initialise(std::stringstream& inputstream){
+
+  bool all_ok = true;
+  std::string line;
+  while(getline(inputstream, line)){
+    if(line.empty()) continue;
+    if(line[0]=='#') continue;
+    std::string key;
+    std::string value;
+    std::stringstream stream(line);
+    if(stream>>key>>value) m_variables[key]=value;
+    else {
+      std::cout<<"Store::Initialise failed to parse line '"<<line<<"'"<<std::endl;
+      all_ok = false;
+    }
+  }
+  
+  return all_ok;
 }
 
 void Store::Print(){
